3ec3e41be90cc1bd7abe1e3120d31c9b84b6e312
huan.gao <2223486623@qq.com>
2022-09-30 10:43:27 +0800

    组合模式
    组合模式

---
+++ src/com/example/structural/composite/Test.java
@@ -0,0 +1,16 @@
+package com.example.structural.composite;
+
+import com.example.structural.composite.bean.Directory;
+
+/**
+ * 组合模式：是用于把一组相似的对象当作一个单一的对象。组合模式依据树形结构来组合对象，用来表示部分以及整体层次。
+ * 测试
+ * @author tianma
+ * @date 2022/ 09/20 17:50:35
+ */
+public class Test {
+    public static void main(String[] args){
+        Directory fileDirectory = new Directory("D:\\opt");
+        fileDirectory.show(" ");
+    }
+}
